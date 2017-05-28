0609d2b12adc9ca67a9757845f44de57181a32e6
pranav-korpal <pkorpal265@gmail.com>
2017-05-28 21:40:55 +0530

    Update Vectorprac1.R

--- Vectorprac1.R
+++ Vectorprac1.R
@@ -1,11 +1,18 @@
+#print odd numbers from 1 to 100
 seq(1,100,2)
+
+#Create the numerical vector with following values 1,2,3,4,5,8,6,2,11 
 vector1<-c(1,2,3,4,5,8,6,2,11)
+
+# Create 3x3 matrix from the vector 
 y<-matrix(vector1,nrow=3,ncol=3)
 
 
+#Consider the following vector a<-c(NA,11:15,NA,NA)  remove all the NA and find the mean of the vector 
 o<-c(NA,11:15,NA,NA)
 mean(o,na.rm=T)
 
+#Consider the vector x=c(”apple”,”banana”,”grape”) 
 x=c("apple","banana","grape")
 sub("a","$",x)
 
